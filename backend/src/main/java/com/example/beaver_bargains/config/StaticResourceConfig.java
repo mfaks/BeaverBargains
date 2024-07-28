aaf96525d35d8a7483d4a81f5f2d422757ede0a5
mfaks <muhammad.faks@gmail.com>
2024-07-28 14:05:41 -0400

    handle static file uplodads for profile images

---
+++ backend/src/main/java/com/example/beaver_bargains/config/StaticResourceConfig.java
@@ -0,0 +1,18 @@
+package com.example.beaver_bargains.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+    }
+}
