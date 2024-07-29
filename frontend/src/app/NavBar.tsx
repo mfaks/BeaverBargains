713e8091be66c03dbc85df14d19fae9abd527751
mfaks <muhammad.faks@gmail.com>
2024-07-29 17:24:48 -0400

    update navbar to fetch profile icons as they are updated

--- frontend/src/app/NavBar.tsx
+++ frontend/src/app/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Link from "next/link"
 import BeaverIcon from "@/components/ui/BeaverIcon"
 import { Button } from "@/components/ui/button"
@@ -29,6 +29,13 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 export default function NavBar() {
   const { isAuthenticated, user, logout } = useAuth()
   const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const [avatarUrl, setAvatarUrl] = useState(user?.profileImage);
+
+  useEffect(() => {
+    if (user && user.profileImage) {
+      setAvatarUrl(user.profileImage);
+    }
+  }, [user]);
 
   const handleLogout = async () => {
     logout()
@@ -40,7 +47,7 @@ export default function NavBar() {
   }
 
   return (
-    <>
+    <div>
       <header className="sticky top-0 z-50 px-4 lg:px-6 h-14 flex items-center justify-between bg-[black] text-[white]">
         <div className="flex items-center">
           <Link href="/" className="flex items-center justify-center" prefetch={false}>
@@ -75,7 +82,7 @@ export default function NavBar() {
               <span className="text-sm font-medium hover:underline underline-offset-4 text-orange-500 flex items-center cursor-pointer">
                 {isAuthenticated && user ? (
                   <Avatar>
-                    <AvatarImage src={user.profileImage || "/default-avatar.png"} alt={user.firstName} />
+                    <AvatarImage src={avatarUrl} alt={user?.firstName} />
                     <AvatarFallback>
                       {user.firstName.charAt(0).toUpperCase()}
                       {user.lastName ? user.lastName.charAt(0).toUpperCase() : ''}
@@ -93,7 +100,7 @@ export default function NavBar() {
               </DropdownMenuLabel>
               <DropdownMenuSeparator />
               {isAuthenticated ? (
-                <>
+                <div>
                   <DropdownMenuItem>
                     <Link href="/account" className="w-full">
                       Account
@@ -123,9 +130,9 @@ export default function NavBar() {
                   <DropdownMenuItem onClick={handleLogout}>
                     Sign Out
                   </DropdownMenuItem>
-                </>
+                </div>
               ) : (
-                <>
+                <div>
                   <DropdownMenuItem>
                     <Link href="/login" className="w-full">
                       Login
@@ -136,7 +143,7 @@ export default function NavBar() {
                       Create Account
                     </Link>
                   </DropdownMenuItem>
-                </>
+                </div>
               )}
             </DropdownMenuContent>
           </DropdownMenu>
@@ -155,6 +162,6 @@ export default function NavBar() {
           </DialogFooter>
         </DialogContent>
       </Dialog>
-    </>
+    </div>
   )
 }
