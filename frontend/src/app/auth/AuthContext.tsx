58fc2275a5204019b22a3c9ce9fd24fb4cbcab22
mfaks <muhammad.faks@gmail.com>
2024-08-01 20:02:41 -0400

    added auth folder

---
+++ frontend/src/app/auth/AuthContext.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react'
+import { User } from '../../types/User'
+import type { AuthContext } from '../../types/AuthContext'
+import { AuthProviderProps } from '../../types/AuthProviderProps'
+
+const AuthContext = createContext<AuthContext | undefined>(undefined)
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [user, setUser] = useState<User | null>(null)
+  const [token, setToken] = useState<string | null>(null)
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user')
+    const storedToken = localStorage.getItem('token')
+    if (storedUser && storedToken) {
+      const userData = JSON.parse(storedUser)
+      setIsAuthenticated(true)
+      setUser(userData)
+      setToken(storedToken)
+    }
+  }, [])
+
+  const login = (userData: User, newToken: string) => {
+    setIsAuthenticated(true)
+    setUser(userData)
+    setToken(newToken)
+    localStorage.setItem('user', JSON.stringify(userData))
+    localStorage.setItem('token', newToken)
+  }
+
+  const logout = () => {
+    setIsAuthenticated(false)
+    setUser(null)
+    setToken(null)
+    localStorage.removeItem('user')
+    localStorage.removeItem('token')
+  }
+
+  const updateUserProfileImage = (newImageUrl: string) => {
+    if (user) {
+      const updatedUser = { ...user, profileImage: newImageUrl }
+      setUser(updatedUser)
+      localStorage.setItem('user', JSON.stringify(updatedUser))
+    }
+  } 
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, user, token, login, logout, updateUserProfileImage }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext)
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
