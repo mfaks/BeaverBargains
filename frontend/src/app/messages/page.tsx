d26d9091c6566379c2fd8794c88dfcdb057fc56d
mfaks <muhammad.faks@gmail.com>
2024-08-02 20:51:31 -0400

    updapted imports

--- frontend/src/app/messages/page.tsx
+++ frontend/src/app/messages/page.tsx
@@ -3,11 +3,11 @@ import Footer from '@/components/ui/Footer'
 
 export default function Messages() {
     return (
-        <div className="flex flex-col min-h-[100dvh] bg-orange-50 text-[#black]">
+        <div className='flex flex-col min-h-[100dvh] bg-orange-50 text-[#black]'>
             <NavBar />
-            <main className="flex-1 container mx-auto px-4 md:px-6 py-12">
-                <div className="flex justify-center mb-6">
-                    <h1 className="text-3xl font-bold text-orange-500 border-b-2 border-orange-500 pb-1">
+            <main className='flex-1 container mx-auto px-4 md:px-6 py-12'>
+                <div className='flex justify-center mb-6'>
+                    <h1 className='text-3xl font-bold text-orange-500 border-b-2 border-orange-500 pb-1'>
                         My Messages
                     </h1>
                 </div>
--- frontend/src/app/orders/page.tsx
+++ frontend/src/app/orders/page.tsx
@@ -1,13 +1,13 @@
-import NavBar from '../NavBar'
-import Footer from '../Footer'
+import NavBar from '@/components/ui/Navbar'
+import Footer from '@/components/ui/Footer'
 
 export default function Orders() {
     return (
-        <div className="flex flex-col min-h-[100dvh] bg-[white] text-[#black]">
+        <div className='flex flex-col min-h-[100dvh] bg-orange-50 text-[#black]'>
             <NavBar />
-            <main className="flex-1 container mx-auto px-4 md:px-6 py-12">
-                <div className="flex justify-center mb-6">
-                    <h1 className="text-3xl font-bold text-orange-500 border-b-2 border-orange-500 pb-1">
+            <main className='flex-1 container mx-auto px-4 md:px-6 py-12'>
+                <div className='flex justify-center mb-6'>
+                    <h1 className='text-3xl font-bold text-orange-500 border-b-2 border-orange-500 pb-1'>
                         My Order History
                     </h1>
                 </div>
