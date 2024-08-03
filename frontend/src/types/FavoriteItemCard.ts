0006f2c4a949fb5ca0b1bd68e5d3c1b8c802c5f3
mfaks <muhammad.faks@gmail.com>
2024-08-03 08:34:32 -0400

    added favrotie card

---
+++ frontend/src/types/FavoriteItemCard.ts
@@ -0,0 +1,7 @@
+import { Item } from './Item'
+
+export interface FavoriteItemCardProps {
+    item: Item
+    onToggleFavorite: (itemId: number) => void
+    getFullImageUrl: (imageUrl: string) => string
+}
