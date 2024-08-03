3e4bffd69e5cd4d7caa8c91625b7876ecbc127b7
mfaks <muhammad.faks@gmail.com>
2024-08-02 21:12:08 -0400

    moved components to seperate folder

--- frontend/src/components/ui/EmptyStateCard.tsx
+++ frontend/src/components/ui/EmptyStateCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
-import { EmptyStateCardProps } from '../types/EmptyStateCardProps'
+import { EmptyStateCardProps } from '../../types/EmptyStateCardProps'
 
 const EmptyStateCard: React.FC<EmptyStateCardProps> = ({
   title,
