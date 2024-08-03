8a5d6cf79ee8ae9b42695f75ad1cf1b717a287a3
mfaks <muhammad.faks@gmail.com>
2024-08-02 20:29:50 -0400

    moved auth around

--- frontend/src/app/AuthProviderWrapper.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { AuthProvider } from './AuthContext'
-
-export function AuthProviderWrapper({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>
-}
