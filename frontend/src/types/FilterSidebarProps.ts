180b35edbfda9dc53093e9a3b8f835cccbe128bf
mfaks <muhammad.faks@gmail.com>
2024-08-13 20:21:42 -0400

    update sidebar props

--- frontend/src/types/FilterSidebarProps.ts
+++ frontend/src/types/FilterSidebarProps.ts
@@ -1,12 +1,13 @@
 export interface FilterSidebarProps {
-    sortOptions: { label: string; value: string }[];
-    priceFilter?: boolean;
-    minPrice: number;
-    maxPrice: number;
-    onSort: (sortBy: string) => void;
-    onPriceFilter: (minPrice: number, maxPrice: number) => void;
-    onDescriptionSearch: (term: string) => void;
-    onTagSearch: (term: string) => void;
-    onTagFilter: (tags: string[]) => void;
-    allTags: string[];
-  }
+  sortOptions: { label: string; value: string }[]
+  priceFilter?: boolean
+  minPrice: number
+  maxPrice: number
+  onSort: (sortBy: string) => void
+  onPriceFilter: (minPrice: number, maxPrice: number) => void
+  onDescriptionSearch: (term: string) => void
+  onTagSearch: (term: string) => void
+  onTagFilter: (tags: string[]) => void
+  allTags: string[]
+  resetTrigger: number
+}
