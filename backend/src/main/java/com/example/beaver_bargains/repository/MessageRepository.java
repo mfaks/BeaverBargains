73ff5b8ce5c52f3b18304c2bbaeb04689e2749a1
mfaks <muhammad.faks@gmail.com>
2024-08-14 17:03:08 -0400

    updated repository code styling

--- backend/src/main/java/com/example/beaver_bargains/repository/MessageRepository.java
+++ backend/src/main/java/com/example/beaver_bargains/repository/MessageRepository.java
@@ -12,7 +12,10 @@ import com.example.beaver_bargains.entity.User;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findByConversationOrderByTimestampAsc(Conversation conversation);
+
     List<Message> findByReceiverAndIsReadFalse(User receiver);
+
     long countByReceiverAndIsReadFalse(User receiver);
+
     List<Message> findByConversationIdAndReceiverAndIsReadFalse(Long conversationId, User receiver);
 }
