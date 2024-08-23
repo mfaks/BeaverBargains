6275b963ac009dd6f0fcacf88315b5faff7bd502
mfaks <muhammad.faks@gmail.com>
2024-08-22 20:55:36 -0400

    updated web config with frontend deployment url

--- backend/src/main/java/com/example/beaver_bargains/config/WebConfig.java
+++ backend/src/main/java/com/example/beaver_bargains/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://beaver-bargains.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
